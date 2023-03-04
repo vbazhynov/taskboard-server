24b29b4718ce1f2adb6d33c51234f11d9af1a36b
vbazhynov <vladbazhynov@gmail.com>
2023-03-04 09:32:32 +0200

    Adding start data to app

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
-import { createServer } from 'http';
-import { Server, Socket } from 'socket.io';
-import { lists } from './assets/mockData';
-import { Database } from './data/database';
-import { CardHandler } from './handlers/card.handler';
-import { ListHandler } from './handlers/list.handler';
-import { ErrorLogger } from './loggers/error.logger';
-import { ReorderService } from './services/reorder.service';
-import { eventLogger } from './handlers/list.handler';
-import { ProxyLogger } from './loggers/reorder.logger';
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import { lists } from "./assets/mockData";
+import { Database } from "./data/database";
+import { CardHandler } from "./handlers/card.handler";
+import { ListHandler } from "./handlers/list.handler";
+import { ErrorLogger } from "./loggers/error.logger";
+import { ReorderService } from "./services/reorder.service";
+import { eventLogger } from "./handlers/list.handler";
+import { ProxyLogger } from "./loggers/reorder.logger";
 
 const PORT = process.env.PORT || 3001;
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
+    origin: "*",
+    methods: ["GET", "POST"],
   },
 });
 
 const db = Database.Instance;
 const reorderService = new ProxyLogger(new ReorderService());
 
-if (process.env.NODE_ENV !== 'production') {
-  db.setData(lists);
-}
+// if (process.env.NODE_ENV !== 'production') {
+db.setData(lists);
+// }
 const onConnection = (socket: Socket): void => {
   new ListHandler(io, db, reorderService).handleConnection(socket);
   new CardHandler(io, db, reorderService).handleConnection(socket);
@@ -34,8 +34,8 @@ const errorLogger = new ErrorLogger();
 
 eventLogger.subscribe(errorLogger);
 
-io.on('connection', onConnection);
+io.on("connection", onConnection);
 
-httpServer.listen(PORT, () => console.log('listening on port: ' + PORT));
+httpServer.listen(PORT, () => console.log("listening on port: " + PORT));
 
 export { httpServer };
